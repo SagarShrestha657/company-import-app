c2e50e2dc2666c527c91759981154b82d48e759e
SagarShrestha657 <shresthasagar657@gmail.com>
2025-05-06 01:22:31 +0530

    added vercel and render link

--- Backend/Routes/CompanyRoute.js
+++ Backend/Routes/CompanyRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { importHandler } from "../controllers/companyController.js";
+import { importHandler } from "../Controllers/CompanyController.js";
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
